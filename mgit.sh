b30df2e9803fae0d0de5b14fb3f05d4cbe4bf9a7
lijiaxuan <18392886693@163.com>
2015-12-13 14:27:20 +0800

    store

--- mgit.sh
+++ mgit.sh
@@ -1,5 +1,6 @@
 #cp -r /home/obsidian/program/ljx /home/obsidian/program/git
 cd ~/program/git
+rm *.tgz
 find ~/program/ljx -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf ljx.tgz
 find ~/virtualbox/xp/code -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf xp.tgz
 git add .
