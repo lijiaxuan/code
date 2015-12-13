25c631cc09b9c9d7518d54bc3b78bd8050209868
lijiaxuan <18392886693@163.com>
2015-12-13 14:36:49 +0800

    store

--- mgit.sh
+++ mgit.sh
@@ -1,8 +1,10 @@
 #cp -r /home/obsidian/program/ljx /home/obsidian/program/git
 cd ~/program/git
 rm *.tgz
-#find ../ljx -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf ljx.tgz
-#find ~/virtualbox/xp/code -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf xp.tgz
+find ../ljx -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf ljx.tgz
+cd /home/obsidian/virtualbox/xp/code
+find ./ -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar /home/obsidian/program/git/czf xp.tgz
+cd ~/program/git
 git add .
 git commit -a -m "store"
 git push -u origin master
