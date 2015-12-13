39760052047ab8a5b939191ce003583788dbeb9e
lijiaxuan <18392886693@163.com>
2015-12-13 14:37:31 +0800

    store

--- mgit.sh
+++ mgit.sh
@@ -3,7 +3,7 @@ cd ~/program/git
 rm *.tgz
 find ../ljx -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf ljx.tgz
 cd /home/obsidian/virtualbox/xp/code
-find ./ -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar /home/obsidian/program/git/czf xp.tgz
+find ./ -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf /home/obsidian/program/git/xp.tgz
 cd ~/program/git
 git add .
 git commit -a -m "store"
