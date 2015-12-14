395513e6d4c7fafd57a2a8c16772fb8ac9cda9bc
lijiaxuan <18392886693@163.com>
2015-12-14 08:22:36 +0800

    store

--- mgit.sh
+++ mgit.sh
@@ -1,4 +1,6 @@
 #cp -r /home/obsidian/program/ljx /home/obsidian/program/git
+dataname=$(date+%Y%m%d)
+echo $datename
 cd ~/program/git
 rm *.tgz
 find ../ljx -type f \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf ljx.tgz
