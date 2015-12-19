17c6de8d7cdb81fd7cc8198c9d1b9cef4c7bd207
lijiaxuan <18392886693@163.com>
2015-12-19 08:49:16 +0800

    store

--- mgit.sh
+++ mgit.sh
@@ -5,7 +5,7 @@ cd ~/program/git
 #rm *.tgz
 find ../ljx -type f -not \( -path '*/matpower/*' -or -path '*/STM32F10x_FWLib/*' \) \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf $tar1name
 cd /home/obsidian/virtualbox/xp/code
-find ./ -type f -not \( -path '*/STM32F10x_FWLib/*' -or -path '*B_GPS*' \) \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf /home/obsidian/program/git/$tar2name
+find ./ -type f -not \( -path '*/STM32F10x_FWLib/*' -or -path '*B_GPS*' -or -path '*GPSControl' \) \( -name "*.c" -or -name "*.h" -or -name "*.py" -or -name "*.nb" -or -name "*.m" -or -name "*.md" \) |xargs tar czf /home/obsidian/program/git/$tar2name
 cd ~/program/git
 git add .
 git commit -a -m "store"
